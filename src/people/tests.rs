8fb6887fafa0fdb5ba1330f282b42f873f9ce04c
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-06-25 14:25:55 +0700

    Improve tests
    Testcases was not properly wrapped with mod, so linter will warn for unused code

--- src/people/tests.rs
+++ src/people/tests.rs
@@ -1,46 +1,49 @@
-use bevy::prelude::{App, Startup};
+#[cfg(test)]
+mod people_plugin_tests {
+    use bevy::app::{App, Startup};
 
-use crate::people::components::{Employment, Person};
-use crate::people::systems::{spawn_person, spawn_person_with_job};
+    use crate::people::components::{Employment, Person};
+    use crate::people::systems::{spawn_person, spawn_person_with_job};
 
-#[test]
-fn did_spawn_5_people_without_job() {
-    let mut app = App::new();
-    app.add_systems(Startup, spawn_person);
-    app.update();
+    #[test]
+    fn did_spawn_5_people_without_job() {
+        let mut app = App::new();
+        app.add_systems(Startup, spawn_person);
+        app.update();
 
-    // 5 people should be spawned
-    assert_eq!(app.world.query::<&Person>().iter(&app.world).len(), 5);
+        // 5 people should be spawned
+        assert_eq!(app.world.query::<&Person>().iter(&app.world).len(), 5);
 
-    app.update();
+        app.update();
 
-    // There should still be 5 people
-    assert_eq!(app.world.query::<&Person>().iter(&app.world).len(), 5);
-}
+        // There should still be 5 people
+        assert_eq!(app.world.query::<&Person>().iter(&app.world).len(), 5);
+    }
 
-#[test]
-fn did_spawn_5_people_with_job() {
-    let mut app = App::new();
-    app.add_systems(Startup, spawn_person_with_job);
-    app.update();
+    #[test]
+    fn did_spawn_5_people_with_job() {
+        let mut app = App::new();
+        app.add_systems(Startup, spawn_person_with_job);
+        app.update();
 
-    // 5 people with job should be spawned
-    assert_eq!(
-        app.world
-            .query::<(&Person, &Employment)>()
-            .iter(&app.world)
-            .len(),
-        5
-    );
+        // 5 people with job should be spawned
+        assert_eq!(
+            app.world
+                .query::<(&Person, &Employment)>()
+                .iter(&app.world)
+                .len(),
+            5
+        );
 
-    app.update();
+        app.update();
 
-    // There should still be 5 people with job
-    assert_eq!(
-        app.world
-            .query::<(&Person, &Employment)>()
-            .iter(&app.world)
-            .len(),
-        5
-    );
+        // There should still be 5 people with job
+        assert_eq!(
+            app.world
+                .query::<(&Person, &Employment)>()
+                .iter(&app.world)
+                .len(),
+            5
+        );
+    }
 }
