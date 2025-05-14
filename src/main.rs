172093457cf87bcc7d8908ae6e6ea43b6806c190
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-05-14 16:58:12 +0700

    add DefaultPlugins to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::App;
+use bevy::DefaultPlugins;
 
 use people::plugins::PeoplePlugin;
 
 mod people;
 
 fn main() {
-    App::new().add_plugins(PeoplePlugin).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PeoplePlugin)
+        .run();
 }
