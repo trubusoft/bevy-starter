c005d899e82e97706d2d6a1bbd9ee378ccdeb8b5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-05-16 13:20:09 +0700

    move LogDiagnosticsPlugin and FrameTimeDiagnosticsPlugin inside DevelopmentPlugin

--- src/development.rs
+++ src/development.rs
@@ -1,14 +1,26 @@
 use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     input::common_conditions::input_just_pressed,
     prelude::{App, IntoScheduleConfigs, KeyCode, Plugin, ResMut, UiDebugOptions, Update},
 };
+use log::info;
 
 const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
 
+/// Anything related to the development cycle should be added on this plugin.
+///
+/// Note:
+/// 1. This plugin is designed not to be included on release.
+/// 2. The [LogDiagnosticsPlugin] itself only logs diagnostic to the console,
+///    while diagnostic is sent by other module such as [FrameTimeDiagnosticsPlugin].
+///    Adding arbitrary diagnostic plugin without adding [LogDiagnosticsPlugin]
+///    won't print anything to the console
 pub struct DevelopmentPlugin;
 
 impl Plugin for DevelopmentPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(LogDiagnosticsPlugin::default());
+        app.add_plugins(FrameTimeDiagnosticsPlugin::default());
         app.add_systems(
             Update,
             toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY)),
@@ -18,4 +30,5 @@ impl Plugin for DevelopmentPlugin {
 
 fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
     options.toggle();
+    info!("Toggling UiDebugOptions");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+#[cfg(feature = "dev")]
 mod development;
 pub mod example;
 
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::{App, DefaultPlugins, Plugin};
 
 pub struct ApplicationPlugin;
@@ -10,12 +10,7 @@ impl Plugin for ApplicationPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins);
 
-        // Enable several development tools for dev builds.
         #[cfg(feature = "dev")]
-        app.add_plugins((
-            development::DevelopmentPlugin,
-            LogDiagnosticsPlugin::default(),
-            FrameTimeDiagnosticsPlugin::default(),
-        ));
+        app.add_plugins(development::DevelopmentPlugin);
     }
 }
