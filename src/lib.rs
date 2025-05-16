c5b1ff93ae969aa473e2bc7edbbe2907dae3536a
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-05-16 15:34:31 +0700

    add window.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@ mod camera;
 #[cfg(feature = "dev")]
 mod development;
 pub mod example;
+mod window;
 
-use bevy::prelude::{App, DefaultPlugins, Plugin};
+use bevy::prelude::{App, DefaultPlugins, Plugin, PluginGroup};
 
 pub struct ApplicationPlugin;
 
 impl Plugin for ApplicationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-            DefaultPlugins,
-            camera::MainCameraPlugin
-        ));
+        // DefaultPlugins and its customization
+        app.add_plugins(DefaultPlugins.set(window::window_plugin()));
+
+        // Project-related plugins
+        app.add_plugins(camera::MainCameraPlugin);
 
         #[cfg(feature = "dev")]
         app.add_plugins(development::DevelopmentPlugin);
