c1aa52fff68d0b3fa4432c4ba5dec27762e08719
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-05-20 16:20:34 +0700

    Disable console on Windows for non development builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Disable console on Windows for non development builds
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
 use bevy::prelude::App;
 use bevy_starter::ApplicationPlugin;
 
