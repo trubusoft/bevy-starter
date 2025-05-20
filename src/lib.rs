c20041fbfcfb40df3e838814b2c89398c7e748e5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-05-20 16:52:44 +0700

    add ApplicationSystemSet

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod physics;
 mod utils;
 mod window;
 
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, IntoScheduleConfigs, Plugin, SystemSet, Update};
 
 pub struct ApplicationPlugin;
 
@@ -18,7 +18,30 @@ impl Plugin for ApplicationPlugin {
         app.add_plugins(camera::CameraPlugin);
         app.add_plugins(physics::PhysicsPlugin);
 
+        // attach application's internal system set to bevy's Update
+        app.configure_sets(
+            Update,
+            (
+                ApplicationSystemSet::TickTimers,
+                ApplicationSystemSet::RecordInput,
+                ApplicationSystemSet::Update,
+            )
+                .chain(),
+        );
+
         #[cfg(feature = "development")]
         app.add_plugins(development::DevelopmentPlugin);
     }
+}
+
+/// High-level groupings of systems for the app in the `Update` schedule.
+/// Make sure to order it in the `configure_sets` call above.
+#[derive(SystemSet, Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
+enum ApplicationSystemSet {
+    /// Tick timers.
+    TickTimers,
+    /// Record player input.
+    RecordInput,
+    /// Do everything else (consider splitting this into further variants).
+    Update,
 }
